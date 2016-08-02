4e30db698df79efd59efa027c2ad4a8ee98b65ba
Sergey Badulin <sbadulin@gmail.com>
2016-08-02 14:38:32 +0300

    add my_all

--- enumerable-methods.rb
+++ enumerable-methods.rb
@@ -23,4 +23,35 @@ module Enumerable
     end
   end
 
+  def my_select
+    if block_given?
+      # create array for selected value
+      ary = []
+      self.my_each { |e|
+        ary << e if (yield e) == true
+      }
+      ary
+    else
+      self.to_enum
+    end
+  end
+
+  def my_all?
+    flag_bool = true
+    for e in self
+      if e.nil? || e == false
+        flag_bool = false
+        break
+      elsif block_given?
+          flag_bool = false if (yield e) == false
+      end
+    end
+    flag_bool
+  end
+
 end
+
+puts %w[cat dog].my_all? { |word| word.length >= 4 }
+puts %w[cat dog].all? { |word| word.length >= 4 }
+puts [nil, true, 99].all?
+puts [nil, true, 99].my_all?
